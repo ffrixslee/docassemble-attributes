da64d0738dd9ed9b33dfb04f1b203d65093027ff
nliyanaam <nliyanaam@smu.edu.sg>
2020-10-10 14:20:18 +0800

    update

--- docassemble/attributes/DADataType.py
+++ docassemble/attributes/DADataType.py
@@ -2,7 +2,7 @@
 
 from docassemble.base.util import DAObject
 
-__all__ = ['DADTBoolean', 'DADTContinue', 'DADTNumber', 'DADTString', 'DADTEmail', 'DADTDate', 'DADTTime', 'DADTDateTime', 'DADTDict', 'DADTCurrency', 'DADTInt', 'DADTMl', 'DADTYesNoRadio', 'DADTObject', 'DADTEnum']
+__all__ = ['DADTBoolean', 'DADTContinue', 'DADTNumber', 'DADTString', 'DADTEmail', 'DADTDate', 'DADTTime', 'DADTDateTime', 'DADTDict', 'DADTCurrency', 'DADTInt', 'DADTMl', 'DADTYesNoRadio', 'DADTEnum']
 
 class DADataType(DAObject):
     def init(self, input_type="default", *pargs, **kwargs):
@@ -156,9 +156,6 @@ class DADTMl(DADataType):
 
 class DADTYesNoRadio(DADataType):
   pass
-
-class DADTObject(DADataType):
-    pass
 
 class DADTEnum(DADataType):
   pass
